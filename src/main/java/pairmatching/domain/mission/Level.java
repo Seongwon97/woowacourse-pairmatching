e5612ebd5815962709a82f4cad0cac2d248b0540
Seongwon97 <osw970325@naver.com>
2022-04-18 01:25:38 +0900

    feat(Level, Mission): 입력값을 통해 올바른 인스턴스를 찾는 메서드 추가

--- src/main/java/pairmatching/domain/mission/Level.java
+++ src/main/java/pairmatching/domain/mission/Level.java
@@ -1,5 +1,8 @@
 package pairmatching.domain.mission;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public enum Level {
 
     LEVEL1("레벨1"),
@@ -12,5 +15,12 @@ public enum Level {
 
     Level(String name) {
         this.name = name;
+    }
+
+    public static Level of(String name) {
+        return Arrays.stream(values())
+                .filter(value -> value.name.equals(name))
+                .findFirst()
+                .orElseThrow(NoSuchElementException::new);
     }
 }
--- src/main/java/pairmatching/domain/mission/Mission.java
+++ src/main/java/pairmatching/domain/mission/Mission.java
@@ -1,23 +1,36 @@
 package pairmatching.domain.mission;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public enum Mission {
 
-    RACING_CAR(Level.LEVEL1),
-    LOTTO(Level.LEVEL1),
-    BASEBALL(Level.LEVEL1),
-    SHOPPING_BASKET(Level.LEVEL2),
-    PAY(Level.LEVEL2),
-    SUBWAY_MAP(Level.LEVEL2),
-    PERFORMANCE_IMPROVEMENT(Level.LEVEL4),
-    DEPLOYMENT(Level.LEVEL4);
+    RACING_CAR("자동차경주", Level.LEVEL1),
+    LOTTO("로또", Level.LEVEL1),
+    BASEBALL("숫자야구게임", Level.LEVEL1),
+    SHOPPING_BASKET("장바구니", Level.LEVEL2),
+    PAY("결제", Level.LEVEL2),
+    SUBWAY_MAP("지하철노선도", Level.LEVEL2),
+    PERFORMANCE_IMPROVEMENT("성능개선", Level.LEVEL4),
+    DEPLOYMENT("배포", Level.LEVEL4);
 
+    private final String name;
     private final Level level;
 
-    Mission(Level level) {
+    Mission(String name, Level level) {
+        this.name = name;
         this.level = level;
     }
 
     public boolean isSameLevel(Mission mission) {
         return this.level == mission.level;
+    }
+
+    public static Mission of(String name, Level level) {
+        return Arrays.stream(values())
+                .filter(value -> value.name.equals(name))
+                .filter(value -> value.level == level)
+                .findFirst()
+                .orElseThrow(NoSuchElementException::new);
     }
 }
